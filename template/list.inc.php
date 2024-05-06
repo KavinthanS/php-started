c43fc086ea23ca816d9230ff908bea9a92a2783f
Kavinthan Singarajah <kavinthan.singarajah@gmail.com>
2024-05-06 17:41:39 +0200

    modification

--- template/list.inc.php
+++ template/list.inc.php
@@ -4,6 +4,28 @@
 
             <?php
             
+                include_once "./model/listlang.inc.php";
+                #print_r($articles);
+                foreach($articles as $key => $value){
+
+                }
             ?>
+            <?php foreach ($articles as $article):?>
+                    <ul>
+                        <li>
+                            <h2><?= $article['title'] ?></h2>
+                        </li>
+                        <li>
+                            <p><?= $article['content'] ?></p>
+                        </li>
+                            <a href="<?= $article['url'] ?>" target="blank">
+                                <?= $article['link'] ?> &raquo;
+                        </a>
+                    </ul>
+            <?php endforeach?>
+
+
+
+
         </section>
 </main>
