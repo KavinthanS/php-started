496677c6e4d54847002df0c7fcbc23b1599107e8
Kavinthan Singarajah <kavinthan.singarajah@gmail.com>
2024-05-06 15:29:13 +0200

    modification

--- template/head.inc.php
+++ template/head.inc.php
@@ -11,3 +11,23 @@
         <link rel="manifest" href="favicon/site.webmanifest">
         <link rel="stylesheet" href="https://giusmili.github.io/PHP-started-2425/css/style.css">
     </head>
+
+<?php
+$_php = [
+    "PHP"=> ":Hypertext Preprocessor, plus connu sous son sigle PHP, est un langage de programmation libre, principalement utilisé pour produire des pages Web dynamiques via un serveur web, mais pouvant également fonctionner comme nimporte quel langage interprété de façon locale. PHP est un langage impératif orienté objet. Wikipédia",
+    "Conçu"=>  "Par : Rasmus Lerdorf",
+    "Dernière version"=> ": 8.3.6 (11 avril 2024)",
+    "Développeurs"=> ": The PHP Group",
+    "Écrit en"=> ": C",
+    "Extension de fichier"=> ": php et phar",
+    "Licence : Licence libre:"=> "Licence PHP",
+    "Paradigmes" => : "Impératif, orienté objet, fonctionnel, procédural, réflexif et interprété",
+
+]
+foreach ($_php as $key => $value) {
+    print_r($_php)
+    foreach ($value as $key_1 => $value_1){
+        print_r($value_1)
+    }
+}
+?>
